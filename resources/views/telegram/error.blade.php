43aa1dd2f510925fe39cc97bf2144ec4f5eeda3e
Dmitrii Povyshev <85509320+AV2x@users.noreply.github.com>
2024-01-26 20:56:05 +0300

    Delete resources directory

--- resources/views/telegram/error.blade.php
+++
@@ -1,8 +0,0 @@
-<b>Новая ошибка</b>
-Текст: <i>{{$e->getMessage()}}</i>
-Файл: <i>{{$e->getFile()}}</i>
-Строка: <i>{{$e->getLine()}}</i>
-@if(Auth::user())
-
-Пользователь: {{Auth::user()->firstname}} {{Auth::user()->lastname}}
-@endif
